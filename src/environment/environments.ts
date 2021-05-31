0719ea0741fcb54658b20b23416b331ba525621d
DanielCendales0821 <danielCendales0821@gmail.com>
2021-05-31 14:36:48 -0500

    commit

--- src/environment/environments.ts
+++ src/environment/environments.ts
@@ -1,5 +1,5 @@
 export enum Environment {
-    BaseURL = 'https://serviasistimos.herokuapp.com/',
+    BaseURL = 'https://serviasistimosbackend.herokuapp.com/',
     clientStorageType = 'SESSION',
     clientEncryptionStorageValue = 'SERVIASISTIMOS',
 }
